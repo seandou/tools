18b645ee3f5700d4d140623e3539c6c5b3f1fa75
Sean Dou <douguohui@gmail.com>
2016-06-15 18:12:52 +0800

    move npm_install_global_modules to commands/npm.js

---
+++ commands/npm.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+
+  install_global_modules: function() {
+    console.log(`
+npm install -g npm cnpm
+npm install -g yo bower gulp
+npm install -g koa-ship-cli
+npm install -g nodemon pm2 shipit-cli
+npm install -g open-doc npminfo npmlist
+npm install -g http-server jianyi
+    `);
+  }
+
+};
--- index.js
+++ index.js
@@ -1,16 +1,9 @@
 'use strict';
 
+const commands = require('./commands');
+
 module.exports = function(t, _) {
 
-  t.alias('npm_global_modules', function() {
-    console.log(`
-      npm install -g npm cnpm
-      npm install -g yo bower gulp
-      npm install -g koa-ship-cli
-      npm install -g nodemon pm2 shipit-cli
-      npm install -g open-doc npminfo npmlist
-      npm install -g http-server jianyi
-    `);
-  });
+  t.alias('npm_install_global_modules', commands.npm.install_global_modules);
 
 };
