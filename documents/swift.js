c213920fd1fd97604876b7abda36464a47f6c9f5
Sean Dou <douguohui@gmail.com>
2016-06-16 11:52:29 +0800

    swift resources

--- documents/swift.js
+++ documents/swift.js
@@ -2,4 +2,5 @@
 
 module.exports = function(_) {
   url_alias('swift_document', 'https://swift.org/documentation/');
+  url_alias('swift_resources', 'https://developer.apple.com/swift/resources/');
 };
