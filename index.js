2fb7dfbe16ce7ba4adf2af517acf8ea25e0c2485
Sean Dou <douguohui@gmail.com>
2016-06-16 16:16:50 +0800

    github trending

--- index.js
+++ index.js
@@ -5,4 +5,5 @@ const path = require('path');
 module.exports = function(_) {
   _.require(path.join(__dirname, 'commands'));
   _.require(path.join(__dirname, 'documents'));
+  _.require(path.join(__dirname, 'sites'));
 };
---
+++ sites/github.js
@@ -0,0 +1,5 @@
+'use strict';
+
+module.exports = function(_) {
+  url_alias('github_trending', 'https://github.com/trending');
+};
