6050df1dbc805c0966937f5f183d131d2b416a8b
Sean Dou <douguohui@gmail.com>
2016-06-16 16:45:24 +0800

    babel-cli

--- commands/npm.js
+++ commands/npm.js
@@ -3,7 +3,7 @@
 let npm_install_global_modules = `
 npm install -g npm cnpm
 npm install -g yo bower gulp
-npm install -g koa-ship-cli
+npm install -g koa-ship-cli babel-cli
 npm install -g nodemon pm2 shipit-cli
 npm install -g open-doc npminfo npmlist
 npm install -g http-server jianyi
