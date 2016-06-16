23badef4e835ce195579be924a46db21fc29203b
Sean Dou <douguohui@gmail.com>
2016-06-16 11:19:09 +0800

    rust document

--- documents/rust.js
+++ documents/rust.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = function(_) {
-  
+  url_alias('rust_document', 'https://www.rust-lang.org/documentation.html');
+  url_alias('rust_by_example', 'http://rustbyexample.com/');
 };
