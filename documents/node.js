3d0d1d0cef33da9ac61fa7002989be44c0b7b877
Sean Dou <douguohui@gmail.com>
2016-06-16 16:41:33 +0800

    awesome nodejs

--- documents/node.js
+++ documents/node.js
@@ -2,4 +2,7 @@
 
 module.exports = function(_) {
   url_alias('node_document', 'https://nodejs.org/dist/latest/docs/api/');
+
+  url_alias('awesome_nodejs', 'https://github.com/sindresorhus/awesome-nodejs');
+  url_alias('node_awesome', 'https://github.com/sindresorhus/awesome-nodejs');
 };
