e05f7531fc436568ae26b65338726cf4fca4e419
Sean Dou <douguohui@gmail.com>
2016-06-21 18:22:37 +0800

    github documentation

--- sites/github.js
+++ sites/github.js
@@ -2,4 +2,5 @@
 
 module.exports = function(_) {
   url_alias('github_trending', 'https://github.com/trending');
+  url_alias('github_developer_documentation', 'https://developer.github.com/v3/');
 };
