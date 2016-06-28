9249eab4b36eb42285e843f963dc69d11cbe938a
Sean Dou <douguohui@gmail.com>
2016-06-28 11:04:41 +0800

    mysql grant sample

--- commands/mysql.js
+++ commands/mysql.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const mysql_grant = `
-grant all privileges on advert.* to 'advert'@'%' identified by 'advert' with grant option;
-grant all privileges on advert.* to 'advert'@'localhost' identified by 'advert' with grant option;
+grant all privileges on dbname.* to 'username'@'%' identified by 'password' with grant option;
+grant all privileges on dbname.* to 'username'@'localhost' identified by 'password' with grant option;
 `;
 
 module.exports = function(_) {
