453fef17b199d2a24b35b7e725cd575b7b42afe6
Sean Dou <douguohui@gmail.com>
2016-06-29 15:02:09 +0800

    github ranking

--- sites/github.js
+++ sites/github.js
@@ -2,5 +2,6 @@
 
 module.exports = function(_) {
   url_alias('github_trending', 'https://github.com/trending');
+  url_alias('github_ranking', 'https://github-ranking.com/');
   url_alias('github_developer_documentation', 'https://developer.github.com/v3/');
 };
