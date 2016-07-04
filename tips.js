6d3d63ddf6c7f88ef601b156cd85b41f4a52dc93
Sean Dou <douguohui@gmail.com>
2016-07-04 14:09:04 +0800

    design

--- tips.js
+++ tips.js
@@ -4,7 +4,7 @@ module.exports = function(_) {
 
   url_alias('mail_exmail', 'http://exmail.qq.com/');
   
-  url_alias('ui_dribbble', 'https://dribbble.com/');
-  url_alias('ui_sketch_app_sources', 'http://www.sketchappsources.com/');
+  url_alias('design_dribbble', 'https://dribbble.com/');
+  url_alias('design_sketch_app_sources', 'http://www.sketchappsources.com/');
 
 };
