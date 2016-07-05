b9a96bdca963a193f82e237792bc520db4aa327b
Sean Dou <douguohui@gmail.com>
2016-07-05 16:30:47 +0800

    sublime text packages

--- tips.js
+++ tips.js
@@ -7,4 +7,13 @@ module.exports = function(_) {
   url_alias('design_dribbble', 'https://dribbble.com/');
   url_alias('design_sketch_app_sources', 'http://www.sketchappsources.com/');
 
+  note_alias('sublime_text_packages', `
+EditorConfig
+DocBlockr
+JavaScript Completions
+JavaScript & NodeJS Snippets
+Node Completions
+EJS
+  `);
+
 };
