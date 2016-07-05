df23d862b3b4a8de836da0aa6839edf0a7d6d253
Sean Dou <douguohui@gmail.com>
2016-07-05 10:05:35 +0800

    ava test

--- commands/npm.js
+++ commands/npm.js
@@ -3,6 +3,7 @@
 let npm_install_global_modules = `
 npm install -g npm cnpm
 npm install -g yo bower gulp
+npm install -g ava
 npm install -g koa-ship-cli babel-cli
 npm install -g nodemon pm2 shipit-cli
 npm install -g open-doc npminfo npmlist
