da4aa428391532ad8c94a308fcff37099dd1df03
Sean Dou <douguohui@gmail.com>
2016-07-06 10:27:32 +0800

    github advanced search

--- sites/github.js
+++ sites/github.js
@@ -3,5 +3,6 @@
 module.exports = function(_) {
   url_alias('github_trending', 'https://github.com/trending');
   url_alias('github_ranking', 'https://github-ranking.com/');
+  url_alias('github_advanced_search', 'https://github.com/search/advanced');
   url_alias('github_developer_documentation', 'https://developer.github.com/v3/');
 };
