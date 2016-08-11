111070c41460eac29c03709939ffdf067cd4598d
Sean Dou <douguohui@gmail.com>
2016-08-11 09:45:53 +0800

    yoman

--- commands/npm.js
+++ commands/npm.js
@@ -2,9 +2,11 @@
 
 let npm_install_global_modules = `
 npm install -g npm cnpm
-npm install -g yo bower gulp grunt-cli
+npm install -g bower gulp grunt-cli
+npm install -g yo generator-webapp
 npm install -g ava
-npm install -g koa-ship-cli babel-cli
+npm install -g koa-ship-cli
+npm install -g babel-cli create-react-app
 npm install -g nodemon pm2 shipit-cli
 npm install -g open-doc npminfo npmlist
 npm install -g http-server pushstate-server sloc
